4b9ca7b0a3469c8afebb2a258f9aabdd31ffe5f6
Tingting Chen <tingtingmacpro@Tingtings-MacBook-Pro.local>
2020-05-25 22:59:10 -0700

    fixed deleting id re-ordering

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Note-Taker
-note taker with express
+
+## Deployed App
+
+
+
+## Description
+
+This application can be used to write, save, and delete notes, supported by the express backend and a `db.json` file that can save and retrieve note data from the html.
+
+
+## HTML routes:
+
+  * GET `/notes` - Should return the `notes.html` file.
+
+  * GET `/` - Should return the `index.html` file
+
+
+## API routes:
+
+  * GET `/api/notes` - reads the `db.json` file and return all saved notes as JSON.
+
+  * POST `/api/notes` - receives a new note to save on the request body, adds it to the `db.json` file, and then returns the new note to the client.
+
+  * DELETE `/api/notes/:id` - receives a query parameter containing the id of a note to delete, and the `db.json` file will be updated accordingly.
+
+
+## Tiny bug
+
+  * The first note is not rendered properly when clicked for some reason... But all following notes will be rendered as "read only" mode when clicked.
+
+  * The edit function has not been requested from the prompt, therefore it hasn't been created to edit existing notes. To compensate, the user can always delete old notes and create a new one to be saved...
+
+
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const util = require('util');
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
-const appendFileAsync = util.promisify(fs.appendFile);
+
 
 // Sets up the Express App
 // =============================================================
@@ -17,25 +17,10 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // app.use(express.static('assets'));
 app.use(express.static('public'));
 app.use(express.static('db'));
-
-// =============================================================
-// const notes = 
-// [
-//     {
-//       title : "Test title",
-//       text : "Test test",
-//       id: 0
-//     },
-//     {
-//       title : "Test title again",
-//       text : "Test test again",
-//       id: 1
-//     }
-
-// ]
 
 
 // Routes
@@ -51,99 +36,80 @@ app.use(express.static('db'));
     res.sendFile(path.join(__dirname, "public/notes.html"));
   });
 
-  // app.get("/api/notes", function(req, res) {
-  //   res.json(notes);
-  // });
- 
-  app.get("/api/notes", function(req, res){
-    res.sendFile(__dirname + "/db/db.json")
+
+// AJAX Calls
+// =============================================================
+
+var notes = [];
+
+//  send json file with all saved notes
+app.get("/api/notes", function(req, res){
+  res.sendFile(__dirname + "/db/db.json")
 });
 
-//   app.get("/api/notes", function(req, res){
-//     fs.readFile(__dirname + "/db/db.json", function(err, data) {
-//     if (err) throw err;
-//     console.log(data);
-//     res.json(data);
-//   });
-// });
-  
-  // app.post("/api/notes", function(req, res) {
-  //   let newId = notes.length;
-  //   let newNote = {title: req.body.title, text: req.body.text, id: newId}
-  //   notes.push(newNote);
-  //   res.json(notes);
-  // });
- var notes = [];
 
+// save the new note to json file
+app.post("/api/notes", function(req, res) {
+  // setting existing notes to "notes" array
   readFileAsync(__dirname + "/db/db.json", "utf8", function(err, data) {
     if (err) {
       throw err;
     }
-
     notes = JSON.parse(data);
   });
 
-  app.post("/api/notes", function(req, res) {
-    readFileAsync(__dirname + "/db/db.json", "utf8", function(err, data) {
-      if (err) {
-        throw err;
-      }
-      notes = JSON.parse(data);
-    });
+  // saving the new note to the "notes" array with a new "id" attribute
+  let newId = notes.length;
+  let newNote = {title: req.body.title, text: req.body.text, id: newId}
+  notes.push(newNote);
 
-    let newId = notes.length;
-    let newNote = {title: req.body.title, text: req.body.text, id: newId}
-    notes.push(newNote);
+  // updating the json file with the new "notes" array
+  writeFileAsync(
+    __dirname + "/db/db.json",
+    JSON.stringify(notes, null, 2),
+    "utf8"
+  ).then(res.send(true));
+});
+
+
+// delete a note from the json file
+app.delete("/api/notes/:id", function(req, res) {
+   // setting existing notes to "notes" array
+  readFileAsync(__dirname + "/db/db.json", "utf8", function(err, data) {
+    if (err) {
+      throw err;
+    }
+    notes = JSON.parse(data);
+
+    // deleting the note item with matching "id" attribute from the "notes" array 
+    var newNotes = [];
+    let deleteId = req.params.id;
+    for (i=0; i < notes.length;  i++){
+      if (i == deleteId){
+        console.log("Deleted ID:" + i);
+      }else{
+        // setting a new order of "id" attributes to the note items of the "newNotes" array
+        let newNote = {title: notes[i].title, text: notes[i].text, id: newNotes.length-1};
+        newNotes.push(newNote)
+      }
+    }
+
+    // updating the json file and the "notes" array with the "newNotes" array
     writeFileAsync(
       __dirname + "/db/db.json",
-      JSON.stringify(notes, null, 2),
+      JSON.stringify(newNotes, null, 2),
       "utf8"
-    ).then(res.send(true));
+    ).then(function(){
+      notes = newNotes;
+      res.send(true)});
   });
 
-
-  var newNotes = [];
-
-  app.delete("/api/notes/:id", function(req, res) {
-    readFileAsync(__dirname + "/db/db.json", "utf8", function(err, data) {
-      if (err) {
-        throw err;
-      }
-      notes = JSON.parse(data);
-
-      let deleteId = req.params.id;
-      var num = notes[deleteId].id;
-
-      for (i=0; i < notes.length;  i++){
-        if (i === num){
-          console.log("Deleted ID:" + i);
-
-          notes.splice(i, 1);
-          console.log(notes);
-        }
-        // else{
-        //   let newNote = {title: notes[i].title, text: notes[i].text, id: newNotes.length-1};
-        //   newNotes.push(newNote)
-        // }
-      }
-
-      writeFileAsync(
-        __dirname + "/db/db.json",
-        JSON.stringify(notes, null, 2),
-        "utf8"
-      ).then(function(){
-        // notes = newNotes;
-        res.send(true)
-      });
-
-    });
-
-  });
+});
 
 
-  
-  // Starts the server to begin listening
-  // =============================================================
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
